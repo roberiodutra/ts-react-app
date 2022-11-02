2c2676e56c08059c1f385fba6b09f8f2ab4fbb41
roberio <roberio-almeida@hotmail.com>
2022-11-01 22:44:02 -0300

    test: when the data is wrong it generates an error

--- src/__tests__/unit/login/login.test.tsx
+++ src/__tests__/unit/login/login.test.tsx
@@ -1,8 +1,13 @@
-import { cleanup, screen } from "@testing-library/react";
+import { cleanup, screen, waitFor } from "@testing-library/react";
 import App from "../../../App";
 import renderWithRouter from "../../utils/renderWithRouter";
 import { getUser, saveUser } from "../../../utils/localStorage";
 import { memberUser } from "../../mocks/userMock";
+import server from "../../mocks/server";
+import { rest } from "msw";
+
+const BASE_URL = "http://localhost:3001";
+const code = { NOT_FOUND: 404 };
 
 describe("Login page tests", () => {
   const renderApp = () =>
@@ -36,5 +41,23 @@ describe("Login page tests", () => {
     saveUser(memberUser);
     const { history } = renderApp();
     expect(history.location.pathname).toBe("/member");
+  });
+
+  it("Login with wrong data trigger an error", async () => {
+    server.use(
+      rest.post(`${BASE_URL}/sign_in`, async (_req, res, ctx) => {
+        return res(ctx.status(404));
+      })
+    );
+    const { user, container } = renderApp();
+    const loginButton = screen.getByTestId("login_button");
+    const emailInput = container.querySelector("#email") as Element;
+    const passwordInput = container.querySelector("#password") as Element;
+
+    await user.type(emailInput, "wrong@email.com");
+    await user.type(passwordInput, "123456");
+    await user.click(loginButton);
+
+    expect(await screen.findByText("User not found")).toBeInTheDocument();
   });
 });
